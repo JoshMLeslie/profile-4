d0670eec3c04f3e1c064c4e4e88dbcdeac3ee6e8
Josh Leslie <hello@joshmleslie.com>
2025-02-25 11:52:54 -0500

    fix 404 routing due to gh static site issues move cname to public to exist in dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router';
 import './App.scss';
 import SidebarComponent from './components/sidebar/Sidebar';
 
 function App() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const redirectPath = sessionStorage.getItem('redirectFromExternal');
+		if (redirectPath) {
+			sessionStorage.removeItem('redirectFromExternal');
+			navigate(redirectPath, {replace: true});
+		}
+	}, [navigate]);
+
 	return (
 		<div id="app-container">
 			<SidebarComponent />

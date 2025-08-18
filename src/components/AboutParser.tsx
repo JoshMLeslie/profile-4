29e0919ac950a980a5833ba13d155855ecd9da66
Josh Leslie <hello@joshmleslie.com>
2025-08-18 15:40:06 -0400

    fix child-needs-key warning

--- src/components/AboutParser.tsx
+++ src/components/AboutParser.tsx
@@ -2,14 +2,17 @@ import { JSX } from 'react';
 import { ContentBlock } from '../types/util';
 import { NoRelLink } from './util';
 
-
 export const RenderContent: React.FC<{blocks: ContentBlock[]}> = ({blocks}) => {
 	const output: JSX.Element[] = [];
-	let paragraph: JSX.Element[] = [];
+	let paragraph: (string | JSX.Element)[] = [];
 
 	const flushParagraph = () => {
 		if (paragraph.length) {
-			output.push(<p>{paragraph}</p>);
+			output.push(
+				<p key={Date.now() + (paragraph[0] as string).slice(0, 20)}>
+					{paragraph}
+				</p>
+			);
 			paragraph = [];
 		}
 	};
@@ -18,30 +21,43 @@ export const RenderContent: React.FC<{blocks: ContentBlock[]}> = ({blocks}) => {
 		switch (block.tag) {
 			case 'p':
 				flushParagraph();
-				output.push(<p>{block.text}</p>);
+				output.push(
+					<p key={Date.now() + block.text.slice(0, 20)}>{block.text}</p>
+				);
 				break;
 
 			case 'p-start':
 			case 'p-continue':
 			case 'p-end':
-				paragraph.push(<>{block.text}</>);
+				paragraph.push(block.text);
 				if (block.tag === 'p-end') {
 					flushParagraph();
 				}
 				break;
 
 			case 'a':
-				paragraph.push(<NoRelLink href={block.href}>{block.text}</NoRelLink>);
+				paragraph.push(
+					<NoRelLink
+						key={Date.now() + block.text.slice(0, 20)}
+						href={block.href}
+					>
+						{block.text}
+					</NoRelLink>
+				);
 				break;
 
 			case 'h2':
 				flushParagraph();
-				output.push(<h2>{block.text}</h2>);
+				output.push(
+					<h2 key={Date.now() + block.text.slice(0, 20)}>{block.text}</h2>
+				);
 				break;
 
 			case 'h3':
 				flushParagraph();
-				output.push(<h3>{block.text}</h3>);
+				output.push(
+					<h3 key={Date.now() + block.text.slice(0, 20)}>{block.text}</h3>
+				);
 				break;
 		}
 	}
